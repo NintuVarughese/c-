69233bdf8cab9ece75d7addd77a88ac18a5e6b24
nintumariya02@gmail.com <nintuvarughese02@gmail.com>
2024-06-06 10:31:50 +0530

    commit

---
+++ print.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace printsomething
+{
+    class Print
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Nintu");
+            Console.WriteLine("I am studing MCA");
+            Console.WriteLine("MCA is masters of computer application");
+        }
+    }
+}
