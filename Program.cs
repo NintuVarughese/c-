f96ee20fbecfb264a1f54e4f728972632c2d3106
nintumariya02@gmail.com <nintuvarughese02@gmail.com>
2024-06-06 10:05:51 +0530

    cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace helloworld
+{
+    public class program1
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("hai");
+        }
+
+    }
+}
+
