b339d6f63419725a190a6eb8d55d19f2ed9651ef
nintumariya02@gmail.com <nintuvarughese02@gmail.com>
2024-06-12 10:44:00 +0530

    c#

---
+++ program.cs
@@ -0,0 +1,30 @@
+namespace dt2
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Android myAndroid = new Android("Samsung", "Galaxy S21", "Android 12");
+
+
+            Console.WriteLine("Initial Android Details:");
+            myAndroid.PrintDetails();
+
+
+            myAndroid.MakeCall("123-456-7890");
+
+
+            Console.WriteLine("\nCharging the battery:");
+            myAndroid.ChargeBattery(70);
+
+            Console.WriteLine("\nUsing the battery:");
+            myAndroid.UseBattery(20);
+
+
+            Console.WriteLine("\nFinal Andorid Details:");
+            myAndroid.PrintDetails();
+
+        }
+    }
+}
